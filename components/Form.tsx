8aa3ed6651d87e65cca0d427a2c948000bc32117
DiegoContreras25 <133098011+DiegoContreras25@users.noreply.github.com>
2025-04-11 17:22:01 +0200

    Add files via upload

---
+++ components/Form.tsx
@@ -0,0 +1,19 @@
+import { FunctionalComponent } from "preact";
+
+const Form: FunctionalComponent = () => {
+    return (
+        <div>
+            <h1>Search Book</h1>
+            <form method="get" action="/search/">
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Introduce un titulo"
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
